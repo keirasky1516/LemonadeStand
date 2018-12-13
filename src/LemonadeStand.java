016a3d6e1af7cb2d75ecd719e319d4da92069677
Keira <keira.plybon.2021@student.lhschools.org>
2018-12-13 08:49:12 -0500

    :)

--- src/LemonadeStand.java
+++ src/LemonadeStand.java
@@ -1,2 +1,97 @@
+import java.util.Scanner;
+
 public class LemonadeStand {
+
+
+    public static void main(String[] argus) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        int sugar;
+        int cups;
+        int lemons;
+        int saleQuantity;
+        double priceOfLemons;
+        double priceOfSugar;
+        double priceOfCup;
+        double salePrice;
+        double profit;
+
+
+
+
+        System.out.println("Data Pool 1");
+
+        System.out.println("Hello! Welcome to my lemonade stand.");
+
+        System.out.println("I bought 25 dollars in supplies and sold 15 dollars in lemonade.");
+
+
+
+
+
+
+        System.out.println("Data Pool 2");
+
+        System.out.println("How many lemons are you purchasing?");
+        keyboard.nextInt();
+        System.out.println("What is the price per lemon?");
+        keyboard.nextDouble();
+
+
+        System.out.println("How many cups are you buying?");
+        keyboard.nextInt();
+        System.out.println("What is the price per cup?");
+        keyboard.nextDouble();
+
+        System.out.println("How many pounds of sugar are you buying?");
+        keyboard.nextInt();
+        System.out.println("What is the price per pound of sugar?");
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
