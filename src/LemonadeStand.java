9e07e81100b05290411ccc83cd0348130653341b
Keira <keira.plybon.2021@student.lhschools.org>
2018-12-18 08:00:53 -0500

    hcj

--- src/LemonadeStand.java
+++ src/LemonadeStand.java
@@ -7,7 +7,7 @@ public class LemonadeStand {
 
         Scanner keyboard = new Scanner(System.in);
 
-<<<<<<< HEAD
+
         String item1;
         String item2;
         String item3;
@@ -50,59 +50,6 @@ public class LemonadeStand {
         keyboard.nextInt();
         System.out.println("How much of item4 do you need");
         System.out.println("What is the cost per item");
-=======
-  String item1;
-  String item2;
-  String item3;
-  int amountOfItemsPurchased;
-  int numberOfItems;
-  Double profit;
-  Double totalSales;
-  Double totalCost;
-  Double costPerItem;
-  int amountOfItem;      
-  
-        
-        System.out.println("Welcome to my manufactoring project");
-        System.out.println("How many days");
-        keyboard.nextInt();
-        
-        System.out.println("How many items are needed");
-        keyboard.nextInt();
-        System.out.println("How much of item1 do you need");
-        keyboard.nextInt();
-        System.out.println("What is the cost per item");
-        keyboard.nextDouble();
-        
-        System.out.println("How much of item2 do you need");
-        keyboard.nextInt();
-        System.out.println("What is the cost per item");
-        keyboard.nextDouble();
-        
-        System.out.println("How much of item3 do you need");
-        keyboard.nextInt();
-        System.out.println("What is the cost per item");
-        
-        System.out.println("How much of item4 do you need");
-        keyboard.nextInt();
-        System.out.println("What is the cost per item");
-        keyboard.nextDouble();
-        
-        System.out.println("My total cost for supplies is"+(costPerItem*amountOfItem));
-        
-        
-        System.out.println("How much did you sell each made item for on Day 1");
->>>>>>> master
-        keyboard.nextDouble();
-        System.out.println("How many did you sell");
-        keyboard.nextInt();
-        
-        
-        
-        
-        
-        
-
 
 
         System.out.println("How much did you sell each made item for on Day 1");
