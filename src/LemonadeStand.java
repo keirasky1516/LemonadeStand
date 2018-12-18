a6fb0c9f62264f76bc3b077c79b053da9ce0f0a1
Keira <keira.plybon.2021@student.lhschools.org>
2018-12-18 15:33:33 -0500

    this class

--- src/LemonadeStand.java
+++ src/LemonadeStand.java
@@ -11,10 +11,20 @@ public class LemonadeStand {
         String item1;
         String item2;
         String item3;
+        double price;
+        int item_1;
+        int item_2;
+        int item_3;
+        int item_4;
+        int sales;
+        double _sales;
+        Double priceItem4;
+        Double priceItem3;
+        Double priceItem2;
+        Double priceItem1;
+        Double amountSold;
         int amountOfItemsPurchased;
         int numberOfItems = 5;
-        Double profit;
-        Double totalSales;
         Double totalCost = null;
         Double costPerItem;
         int amountOfItem;
@@ -22,6 +32,11 @@ public class LemonadeStand {
         totalCost = costPerItem;
         amountOfItem = numberOfItems;
         numberOfItems = amountOfItem;
+        amountSold=totalCost;
+
+
+
+
 
 
         System.out.println("Welcome to my manufacturing project");
@@ -31,31 +46,34 @@ public class LemonadeStand {
         System.out.println("How many items are needed");
         keyboard.nextInt();
         System.out.println("How much of item1 do you need");
-        keyboard.nextInt();
+        item_1=keyboard.nextInt();
         System.out.println("What is the cost per item");
-        keyboard.nextDouble();
+        priceItem1=keyboard.nextDouble();
 
 
 
     System.out.println("How much of item2 do you need");
-        keyboard.nextInt();
+        item_2=keyboard.nextInt();
         System.out.println("What is the cost per item");
-        keyboard.nextDouble();
+       priceItem2= keyboard.nextDouble();
 
         System.out.println("How much of item3 do you need");
-        keyboard.nextInt();
+        item_3=keyboard.nextInt();
         System.out.println("What is the cost per item");
-        keyboard.nextDouble();
+        priceItem3=keyboard.nextDouble();
 
-        keyboard.nextInt();
+
         System.out.println("How much of item4 do you need");
+        item_4=keyboard.nextInt();
         System.out.println("What is the cost per item");
+        priceItem4=keyboard.nextDouble();
 
+        double itemTotal=priceItem1*item_1+priceItem2*item_2+item_3*priceItem3+item_4*priceItem4;
 
-        System.out.println("How much did you sell each made item for on Day 1");
-        keyboard.nextDouble();
-        System.out.println("How many did you sell");
-        keyboard.nextInt();
+        double totalSales=totalCost*amountSold;
+        double profit= totalSales-itemTotal;
+        System.out.println("Your total profit is $"+profit);
+
 
 
 
