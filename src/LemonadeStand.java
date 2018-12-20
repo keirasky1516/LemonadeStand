b5cbef028e68c11d1369437086c89217149a5d27
keirasky1516 <42771455+keirasky1516@users.noreply.github.com>
2018-12-19 19:07:26 -0600

    Update LemonadeStand.java
    ngfklj

--- src/LemonadeStand.java
+++ src/LemonadeStand.java
@@ -80,6 +80,46 @@ public class LemonadeStand {
         double totalSales=price*_sales;
         double profit= totalSales-itemTotal;
         System.out.println("Your total profit is $"+profit);
+        
+        
+        System.out.println("Day 2");
+        
+         System.out.println("How many items are needed");
+        keyboard.nextInt();
+        System.out.println("How much of item1 do you need");
+        item_1=keyboard.nextInt();
+        System.out.println("What is the cost per item");
+        priceItem1=keyboard.nextDouble();
+
+
+
+    System.out.println("How much of item2 do you need");
+        item_2=keyboard.nextInt();
+        System.out.println("What is the cost per item");
+       priceItem2= keyboard.nextDouble();
+
+        System.out.println("How much of item3 do you need");
+        item_3=keyboard.nextInt();
+        System.out.println("What is the cost per item");
+        priceItem3=keyboard.nextDouble();
+
+
+        System.out.println("How much of item4 do you need");
+        item_4=keyboard.nextInt();
+        System.out.println("What is the cost per item");
+        priceItem4=keyboard.nextDouble();
+
+        System.out.println("How many were sold");
+        _sales=keyboard.nextInt();
+        System.out.println("For what price each");
+        price=keyboard.nextDouble();
+
+        double itemTotal=priceItem1*item_1+priceItem2*item_2+item_3*priceItem3+item_4*priceItem4;
+
+        double totalSales=price*_sales;
+        double profit= totalSales-itemTotal;
+        System.out.println("Your total profit is $"+profit);
+
 
 
 
